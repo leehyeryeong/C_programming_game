efc79dc152dd647b8dbf51915e60c3d5aace3832
leehyeryeong <w2108@e-mirim.hs.kr>
2023-01-21 22:48:54 +0900

    포인터를 이용한 물고기 키우기 게임 2

--- pointer/fish_grow.c
+++ pointer/fish_grow.c
@@ -14,6 +14,7 @@ int* cursor;
 
 void initDate();
 void printFishes();
+void decreaseWater(long elapsedTime);
 
 int main() {
 	long startTime = 0; //게임 시작 시간
@@ -43,6 +44,35 @@ int main() {
 		//직전 물 준 시간(마지막으로 물 준 시간) 이후로 흐른 시간
 		prevElapsedTime = totalElapsedTime - prevElapsedTime;
 		printf("최근 경과 시간: %ld 초\n", prevElapsedTime);
+	
+		//어항의 물을 감소(증발)
+		decreaseWater(prevElapsedTime);
+
+		//사용자가 입력한 어항에 물을 준다
+		//1. 어항의 물이 0이면? 물을 주지 않는다... 이미 물고기가... 흑흑ㅠㅠ
+		if (cursor[num - 1] <= 0) {
+			printf("%d 번 물고기는 이미 죽었습니다... 물을 주지 않습니다.\n", num);
+		}
+
+		//2. 어항의 물이 9이 아닌 경우? 물을 준다! 100을 넘지 않는지 체크
+		else if (cursor[num - 1] <= 100) {
+			//물을 준다
+			printf("%d 번 어항에 물을 줍니다.\n\n", num);
+			cursor[num - 1] += 1; //cursor[num-1] = cursor[num-1]+1
+		}
+
+		//레벨업을 할 건지 확인(레벨업은 20초마다 한번씩 수행)
+		if (totalElapsedTime / 20 > level - 1) {
+			//레벨업
+			level++; //level: 1 -> level: 2 -> level: 3...
+			printf(" *** 축 레벨업! 기존 %d 레벨에서 %d 레벨로 업그레이드 ***\n\n", level - 1, level);
+		
+			//최종 레벨: 5
+			if (level == 5) {
+				printf("\n\n축하합니다. 최고 레벨을 달성하였습니다. 게임을 종료합니다.\n\n");
+				exit(0);
+			}
+		}
 	}
 
 	return 0;
@@ -60,4 +90,12 @@ void printFishes() {
 		printf(" %3d ", arrayFish[i]);
 	}
 	printf("\n\n");
+}
+void decreaseWater(long elapsedTime) {
+	for (int i = 0; i < 6; i++) {
+		arrayFish[i] -= (level * 3 * (int)elapsedTime); //3: 난이도 조절을 위한 값
+		if (arrayFish[i] < 0) {
+			arrayFish[i] = 0;
+		}
+	}
 }
