84f61e378bfc8cb699131af91b04026e9491c248
leehyeryeong <w2108@e-mirim.hs.kr>
2023-01-27 23:47:51 +0900

    다차원 배열을 이용한 동물 카드 뒤집기 게임 1

---
+++ multi_array/card_flip.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+
+//10마리의 서로 다른 동물(각 카드 2장씪)
+//사용자로부터 2개의 입력값을 받아서 -> 같은 동물 찾으면 카드 뒤집기
+//모든 동물 쌍을 찾으면 게임 종료
+//총 실패 횟수 알려주기
+
+int arrayAnimal[4][5]; //카드 지도(20장의 카드)
+char* strAnimal[10];
+
+void initAnimalArray();
+void initAnimalName();
+void shuffleAnimal();
+
+int main() {
+	srand(time(NULL));
+
+	initAnimalArray();
+	initAnimalName();
+
+	shuffleAnimal();
+
+	return 0;
+}
+void initAnimalArray() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			arrayAnimal[i][j] = -1;
+		}
+	}
+}
+void initAnimalName() {
+	strAnimal[0] = "원숭이";
+	strAnimal[1] = "하마";
+	strAnimal[2] = "강아지";
+	strAnimal[3] = "고양이";
+	strAnimal[4] = "돼지";
+
+	strAnimal[5] = "코끼리";
+	strAnimal[6] = "기린";
+	strAnimal[7] = "낙타";
+	strAnimal[8] = "타조";
+	strAnimal[9] = "호랑이";
+}
+void shuffleAnimal() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 2; j++) {
+			//강의 7분 32초부터
+		}
+	}
+}
