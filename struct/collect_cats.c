62cb6a5dec03b06c3c4b242fe39170962915d703
leehyeryeong <w2108@e-mirim.hs.kr>
2023-02-12 23:58:32 +0900

    구조체를 이용한 고양이 수집하기 게임 2

--- struct/collect_cats.c
+++ struct/collect_cats.c
@@ -25,6 +25,8 @@ int collection[5] = { 0, 0, 0, 0, 0 };
 CAT cats[5];
 
 void initCats(); //고양이 정보 초기화
+void printCat(int selected);
+int checkCollection();
 
 int main() {
 	srand(time(NULL));
@@ -34,8 +36,14 @@ int main() {
 		printf("두근두근~! 어느 고양이의 집사가 될까요?\n 아무 키나 눌러 확인하세요!");
 		getchar();
 
-		int selected = rand() * 5; //0~4 사이의 숫자 반환
-		printCat(selected);
+		int selected = rand() % 5; //0~4 사이의 숫자 반환
+		printCat(selected); //고양이 정보 출력
+		collection[selected] = 1; //고양이 뽑기 처리
+
+		int collectAll = checkCollection();
+		if (collectAll == 1) {
+			break;
+		}
 	}
 
 	return 0;
@@ -71,9 +79,33 @@ void printCat(int selected) {
 	printf("이름		: %s\n", cats[selected].name);
 	printf("나이		: %d\n", cats[selected].age);
 	printf("특징(성격)	: %s\n", cats[selected].character);
-	printf("레벨		: %d\n");
+	printf("레벨		: ");
 
 	for (int i = 0; i < cats[selected].level; i++) {
 		printf("%s", "★");
+	}
+}
+int checkCollection() {
+	//현재 보유한 고양이 목록도 출력
+	//다 모았는지 여부를 반환
+	int collectAll = 1;
+	printf("\n\n=== 보유한 고양이 목록이에요 ===\n\n");
+
+	for (int i = 0; i < 5; i++) {
+		if (collection[i] == 0) {
+			//고양이 수집 X
+			printf("%10s", "(빈 박스)");
+			collectAll = 0; //다 모으지 못한 상태
+		}
+		else {
+			//고양이 수집 O
+			printf("%10s", cats[i].name);
+		}
+	}
+
+	printf("\n==================================================\n\n");
+
+	if (collectAll) {
+		printf("\n\n축하합니다! 모든 고양이를 다 모았어요. 열심히 키워주세요!\n");
 	}
 }
